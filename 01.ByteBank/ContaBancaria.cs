8fb422723cfb24cbba014dd2eb4ce641cc7fdedd
marcelooliveira <mclricardo@gmail.com>
2018-08-13 15:17:17 -0300

    .

--- 01.ByteBank/ContaBancaria.cs
+++ 01.ByteBank/ContaBancaria.cs
@@ -14,10 +14,10 @@ namespace _01.ByteBank
 
         public void SacarDinheiro(decimal quantia)
         {
-            ///<image url="$(ProjectDir)\img1.png"/>
-            ///<image url="$(ProjectDir)\img2.png"/>
-            ///<image url="$(ProjectDir)\img3.png"/>
-            ///
+            ///<image url="$(ProjectDir)\img.png"/>
+            ///<image url="$(ProjectDir)\img.png"/>
+            ///<image url="$(ProjectDir)\img.png"/>
+
             if (TemSaldoSuficiente(quantia))
             {
                 Sacar(quantia);
