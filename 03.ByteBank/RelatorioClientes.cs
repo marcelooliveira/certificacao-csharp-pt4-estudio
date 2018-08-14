b464d8dc11d92ba48c3e2eae5bb307d1928747fc
marcelooliveira <mclricardo@gmail.com>
2018-08-14 18:16:01 -0300

    .

---
+++ 03.ByteBank/RelatorioClientes.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace _03.ByteBank
+{
+    class RelatorioClientes
+    {
+        //public static void ImprimirListagemClientes()
+        //{
+        //    using (var leitor = new StreamReader("Clientes.txt"))
+        //    {
+        //        // lê o cabeçalho
+        //        string linha = leitor.ReadLine();
+        //        Console.WriteLine(linha); //imprime cabeçalho
+
+        //        //lê primeira linha de dados
+        //        linha = leitor.ReadLine();
+        //        //verifica fim de arquivo
+        //        if (linha == null) { return; }
+
+        //        //imprime a primeira linha de dados
+        //        var campos = linha.Split(',');
+        //        Console.WriteLine(
+        //            $"{int.Parse(campos[0])}, {campos[1]}" +
+        //            $", {campos[2]}, {campos[3]}" +
+        //            $", {decimal.Parse(campos[4])}, {campos[5]}");
+        //    }
+        //}
+
+        public static void ImprimirListagemClientes()
+        {
+            using (var leitor = new StreamReader("Clientes.txt"))
+            {
+                // lê o cabeçalho
+                string linha = leitor.ReadLine();
+                Console.WriteLine(linha); //imprime cabeçalho
+
+                //lê primeira linha de dados e verifica fim de arquivo
+                while ((linha = leitor.ReadLine()) != null)
+                {
+                    //imprime a primeira linha de dados
+                    var campos = linha.Split(',');
+                    Console.WriteLine(
+                        $"{int.Parse(campos[0])}, {campos[1]}" +
+                        $", {campos[2]}, {campos[3]}" +
+                        $", {decimal.Parse(campos[4])}, {campos[5]}"); 
+                }
+            }
+        }
+    }
+}
