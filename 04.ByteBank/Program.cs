714c2b31a19d471bdbb54bd9a3bee37e55c5a7d9
Marcelo Oliveira <mclricardo@gmail.com>
2018-08-15 17:29:32 -0300

    979-CertificacaoCSharpParte4-video4.1-Substituindo For por While.trec

--- 04.ByteBank/Program.cs
+++ 04.ByteBank/Program.cs
@@ -30,13 +30,20 @@ namespace _04.ByteBank
             //FATORIAL DE 0                      = 1 
 
             int fatorial = 1;
-            int fator = numero;
+            //int fator = numero; //inicializacao
 
-            while (fator >= 1)
+            //while (fator >= 1) //condicao
+            //{
+            //    fatorial = fatorial * fator;
+            //    fator = fator - 1; //decremento
+            //}
+
+            for (int fator = numero; fator >= 1; fator--)
             {
-                fatorial = fatorial * fator;
-                fator = fator - 1;
+                fatorial *= fator;
             }
+
+
             System.Console.WriteLine($"fatorial de {numero} é {fatorial}");
 
             return fatorial;
