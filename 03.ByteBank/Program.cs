97afb3423935f6e774f66430bb099f87722d2ea0
Marcelo Oliveira <mclricardo@gmail.com>
2018-08-15 15:39:00 -0300

    979-CertificacaoCSharpParte4-video3.1-Repetindo com While

--- 03.ByteBank/Program.cs
+++ 03.ByteBank/Program.cs
@@ -7,6 +7,14 @@ namespace _03.ByteBank
     {
         static void Main(string[] args)
         {
+            GetFatorial(5);
+            GetFatorial(4);
+            GetFatorial(3);
+            GetFatorial(2);
+            GetFatorial(1);
+            GetFatorial(0);
+
+            RelatorioClientes.ImprimirListagemClientes();
         }
 
         private static int GetFatorial(int numero)
@@ -19,7 +27,13 @@ namespace _03.ByteBank
             //FATORIAL DE 0                      = 1 
 
             int fatorial = 1;
-            
+            int fator = numero;
+
+            while (fator >= 1)
+            {
+                fatorial = fatorial * fator;
+                fator = fator - 1;
+            }
             System.Console.WriteLine($"fatorial de {numero} é {fatorial}");
 
             return fatorial;
